540f9a7372d1585f7410114b28ad35fcee1dcfd8
bliutwo <bliutwo@cs.stanford.edu>
2020-05-13 09:31:02 -0700

    13/05/2020 09:31:02

---
+++ git_new_comp.sh
@@ -0,0 +1,7 @@
+sudo apt install git
+git config --global user.email "bliutwo@cs.stanford.edu"
+git config --global user.name "bliutwo"
+git config --global push.default simple
+git config --global core.editor "vim"
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
